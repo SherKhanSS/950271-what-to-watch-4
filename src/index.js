898f6d24809bd83889a246f11e841f8d0708dcb9
SherKhan <gurovg@gmail.com>
2020-06-09 13:36:59 +0300

    fulfills the module1-task3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, hello, hello!`;
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const film = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  date: 2014,
+};
+
+ReactDOM.render(
+    <App
+      filmInfo={film}
+    />,
+    document.querySelector(`#root`)
+);
