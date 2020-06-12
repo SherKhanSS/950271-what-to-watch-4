068d9949a7854fc4d4ed957cf06edb118a039855
SherKhan <gurovg@gmail.com>
2020-06-12 14:29:40 +0300

    fulfills module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {filmInfo} = props;
+
+  const {filmInfo, filmTitles} = props;
 
   return (
-    <Main filmInfo={filmInfo}/>
+    <Main
+      filmInfo={filmInfo}
+      filmTitles={filmTitles}
+    />
   );
+};
+
+App.propTypes = {
+  filmInfo: PropTypes.object.isRequired,
+  filmTitles: PropTypes.array.isRequired,
 };
 
 export default App;
